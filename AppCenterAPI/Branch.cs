0877dd810c68a6aa1b8e088b369815fbb014377e
Aleksandar Popov <v-apopov@microsoft.com>
2022-06-03 20:44:09 +0200

    Add project files.

---
+++ AppCenterAPI/Branch.cs
@@ -0,0 +1,59 @@
+﻿namespace AppCenterAPI
+{
+    public class Branch
+    {
+        public string name { get; set; }
+        public Commit commit { get; set; }
+        public bool @protected { get; set; }
+        public Protection protection { get; set; }
+        public string protection_url { get; set; }
+    }
+
+    public class Commit
+    {
+        public string sha { get; set; }
+        public string url { get; set; }
+    }
+
+    public class LastBuild
+    {
+        public int id { get; set; }
+        public string buildNumber { get; set; }
+        public DateTime queueTime { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime finishTime { get; set; }
+        public DateTime lastChangedDate { get; set; }
+        public string status { get; set; }
+        public string result { get; set; }
+        public string reason { get; set; }
+        public string sourceBranch { get; set; }
+        public string sourceVersion { get; set; }
+        public List<string> tags { get; set; }
+        public Properties properties { get; set; }
+    }
+
+    public class Properties
+    {
+    }
+
+    public class Protection
+    {
+        public bool enabled { get; set; }
+        public RequiredStatusChecks required_status_checks { get; set; }
+    }
+
+    public class RequiredStatusChecks
+    {
+        public string enforcement_level { get; set; }
+        public List<object> contexts { get; set; }
+        public List<object> checks { get; set; }
+    }
+
+    public class Root
+    {
+        public Branch branch { get; set; }
+        public bool configured { get; set; }
+        public LastBuild lastBuild { get; set; }
+        public string trigger { get; set; }
+    }
+}
---
+++ AppCenterAPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using AppCenterAPI;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+using (HttpClient client = new HttpClient())
+{
+	client.DefaultRequestHeaders.Accept.Add(
+		new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+
+	client.DefaultRequestHeaders.Add("X-API-Token", "69a22fa24e39afb87391c9167aebcd5df79bd401");
+	using (HttpResponseMessage response = await client.GetAsync("https://api.appcenter.ms/v0.1/apps/aleksandar_popov/Akvelon-home-task/branches"))
+	{
+		response.EnsureSuccessStatusCode();
+		var responseBody = await response.Content.ReadAsStringAsync();
+		var test = JsonConvert.DeserializeObject<List<Root>>(responseBody);
+		foreach (var bran in test)
+		{
+			Console.WriteLine("{0} build {1}.", bran.branch.name,bran.lastBuild.status);
+		}
+
+	}
+}
